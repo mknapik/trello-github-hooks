0b0ccca3d34d93bc6f81adeee1b058139b8bb3b6
Michał Knapik <mknapik@student.agh.edu.pl>
2013-09-30 17:24:17 +0200

    default Rails 4.0 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TrelloGithubHooks::Application.config.secret_key_base = 'dcca6bd9ee34f817b40db3e6f3419ffde9ee465d50b468de32f6e71c446dec119b4de2f4150d3e2c49dc0f5f513964697294133968b1e2c94251dc393b9229c3'
