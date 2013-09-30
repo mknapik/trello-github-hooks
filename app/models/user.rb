f6f349439e3f2c6cf29e2599e866b06661211db1
Michał Knapik <mknapik@student.agh.edu.pl>
2013-09-30 17:56:58 +0200

    add and setup devise

---
+++ app/models/user.rb
@@ -0,0 +1,33 @@
+# == Schema Information
+# Schema version: 20130930155414
+#
+# Table name: users
+#
+# *id*::                     <tt>integer, not null, primary key</tt>
+# *email*::                  <tt>string(255), default(""), not null, indexed</tt>
+# *encrypted_password*::     <tt>string(255), default(""), not null</tt>
+# *reset_password_token*::   <tt>string(255), indexed</tt>
+# *reset_password_sent_at*:: <tt>datetime</tt>
+# *remember_created_at*::    <tt>datetime</tt>
+# *sign_in_count*::          <tt>integer, default(0), not null</tt>
+# *current_sign_in_at*::     <tt>datetime</tt>
+# *last_sign_in_at*::        <tt>datetime</tt>
+# *current_sign_in_ip*::     <tt>string(255)</tt>
+# *last_sign_in_ip*::        <tt>string(255)</tt>
+# *created_at*::             <tt>datetime</tt>
+# *updated_at*::             <tt>datetime</tt>
+#
+# Indexes
+#
+#  index_users_on_email                 (email) UNIQUE
+#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
+#--
+# == Schema Information End
+#++
+
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+end
