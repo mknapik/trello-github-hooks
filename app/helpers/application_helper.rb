491517321cbd150f06023a13d3a39a6b60ee1626
Michał Knapik <mknapik@student.agh.edu.pl>
2013-09-30 18:15:27 +0200

    add slim devise views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,12 @@
 module ApplicationHelper
+  def alert_class(alert_type)
+    alert_type = {
+        alert: 'error',
+        notice: 'info'
+    }.fetch(alert_type, alert_type.to_s)
+    "alert-#{alert_type}"
+  end
+
   def nav_link_to(link_text, link_path)
     class_name = current_page?(link_path) ? 'active' : ''
 
