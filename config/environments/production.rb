64c2fe0ba35f13b2bb605dd157ec5964ad8e0ddb
Michał Knapik <mknapik@student.agh.edu.pl>
2013-10-01 16:31:42 +0200

    remove analytics from GA plugin
    added by accident

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,6 +50,7 @@ TrelloGithubHooks::Application.configure do
 
   # Use a different logger for distributed setups.
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
+  config.logger = Logger.new(STDOUT)
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
